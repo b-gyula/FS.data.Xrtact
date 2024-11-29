efc47fe54ef0c080c25f781eefd48fcca497cdfa
Gyula Bibernáth <27079815+b-gyula@users.noreply.github.com>
2024-11-29 13:15:32 +0100

    - Using t! in the -x parameter limit the generation to tractors with only the first engine variant - Prefixing path with : will prevent adding `/ data /...`

--- src/FSXtract.scala
+++ src/FSXtract.scala
@@ -6,24 +6,30 @@ import pprint.pprintln
 object FSXtract {
 	var logEachObject = false
 
-	@main
-	def main(@arg(short = 'p', doc = "path of the game data folder")
-				gamePath: String = "D:/Game/FS.22",
-				@arg(short = 'x', doc = "type of xtract: f - fruit prices, t - tractors, p - productions")
+	@main(doc="Extracts fruit + fill type prices into fruits.csv and price.csv, productions into prods.csv and " +
+		"tractors into tractors.csv from the game install folder specified by the first (p) parameter.\n" +
+		"Individual types can be defined by the second (x) parameter e.g -x t -> Extract tractors only\n" +
+		"\nDo not use multiple values in -x with : prefixed path!")
+	def main(@arg(short = 'p', doc = "path of the game install folder. Prefixing with : the path is used as is and not added `/ data / ...`")
+				gamePath: String = "D:/Game/FS.25",
+				@arg(short = 'x', doc = "type of xtract: f - fruit prices, t - tractors, p - productions. t! will generate tractors with only the first engine variant")
 				xtract: String = "ftp",
 				@arg(short = 'v', doc = "Log extra information during processing")
 				verbose: Flag): Unit = {
-		val dataPath = os.Path(gamePath) / "data"
+		val directPath = gamePath.startsWith(":")
+		val dataPath = if(directPath) os.Path(gamePath.substring(1))
+										 else os.Path(gamePath) / "data"
 		logEachObject = verbose.value
 		if(xtract.contains('t')) {
-			printOut(Tractors, dataPath / "vehicles", "tractors.csv")
+			Tractors.firstEngineOnly = xtract.contains("t!")
+			printOut(Tractors, if (directPath) dataPath else dataPath / "vehicles", "tractors.csv")
 		}
 		if(xtract.contains('f')) {
-			printOut(FillTypes, dataPath / "maps", "price.csv")
-			printOut(FruitTypes, dataPath / "maps", "fruits.csv")
+			printOut(FillTypes, if(directPath) dataPath else dataPath / "maps", "prices.csv")
+			printOut(FruitTypes, if(directPath) dataPath else dataPath / "maps", "fruits.csv")
 		}
 		if(xtract.contains('p')) {
-			printOut(Productions, dataPath / "placeables", "prods.csv")
+			printOut(Productions, if (xtract == "p/") os.Path(gamePath) else dataPath / "placeables", "prods.csv")
 		}
 	}
 
--- src/Tractors.scala
+++ src/Tractors.scala
@@ -12,7 +12,8 @@ import scala.collection.mutable.Buffer
 object Tractors extends Extractor {
 	type T = Tractor
 	val fileName=""
-
+	/** Return each tractor with only the first engine variant*/
+	var firstEngineOnly = false
 	case class Engine(name: String, hp:Int, price: Int) {
 		def toCsv: String = str(name, hp.toString, price.toString)
 	}
@@ -27,7 +28,7 @@ object Tractors extends Extractor {
 							 engines: Buffer[Engine] = Buffer()
 							)
 		extends Named {
-		val name = "" // Just for named
+		val name = "" // Just for Named
 		override
 		def toCsvRow: String = engines.foldLeft(new StringBuilder()) (
 			(sb, e) => sb ++= toCsv.format(e.toCsv) + nl
@@ -37,14 +38,22 @@ object Tractors extends Extractor {
 		def toCsv: String = str(brand, series, "%s", maxSpeed.toString, cat, extra.mkString("\"",", ","\""))
 	}
 	override
-	val headers = "brand,series,name,hp,price,maxSpeed,cat,extra"
+	val headers = "brand,series,name,hp,price,maxSpeed,cat,extras"
+
 	override
 	def collect(path: os.Path): Seq[Tractor] = {
 		os.walk(path)
 			.filter(p => if(!os.isDir(p) && p.ext == "xml") {
-				val prefix = os.read(p, UTF_8, 0, 900)
-				prefix.contains(" type=\"tractor")
-					&& prefix.contains("<function>$l10n_function_tractor</function>")
+				val prefix: String = os.read(p, UTF_8, 0, 1300)
+				var ttractor = prefix.indexOf(" type=\"tractor") + 14
+				if(ttractor > 14 && ( prefix.startsWith("\"", ttractor)
+										 || prefix.startsWith("ReverseDriving\"", ttractor))) {
+					// TODO implement include with update for JohnDeere 9RX in D:\game\FS.25\data\vehicles\johnDeere\series9RX\series9RX.xml
+					ttractor = prefix.indexOf("<function>$l10n_function_tractor</function>", ttractor)
+					if(ttractor > 0) {
+						prefix.indexOf("<category>tractors", ttractor + 43) > 0
+					} else false
+				} else false
 			} else false )
 			.foldLeft(Seq.empty[Tractor]) ((lst, f) => {
 				println("Reading Tractor from " + f)
@@ -61,28 +70,32 @@ object Tractors extends Extractor {
 		case Vector("vehicle", "storeData") => withErrorLog(p) {
 				(e: Elem) => {
 					val cat = (e \ "category").head.text
-					val series: String = e \ "name"
-					if (cat.startsWith("tractor")) {
-						base = //Some(
-							Tractor(e \ "brand"
-								, if (series.endsWith(" Series")) series.substring(0, series.length - 7) else series
-								, e \ "price"
-								, e \ "specs" \ "power"
-								, e \ "specs" \ "maxSpeed"
-								, if (cat.length > 8) cat.substring(8) else ""
-							)
-						//)
-						if ((e \ "functions" \ "function").exists(_.text == "$l10n_function_tractorFrontloader")) {
-							base.extra += "front loader"
-						}
-						base
-					} else null
+					var series: String = e \ "name"
+					if(series.startsWith( "$l10n_shopItem_seriesX") ) {
+						series = e \ "name" \@ "params"
+					} else if (series.endsWith(" Series")) {
+						series = series.substring(0, series.length - 7)
+					}
+					base = Tractor( e \ "brand"
+										,series
+										,e \ "price"
+										,e \ "specs" \ "power"
+										,e \ "specs" \ "maxSpeed"
+										,if (cat.length > 8) cat.substring(8) else ""
+					)
+					if ((e \ "functions" \ "function").exists(_.text == "$l10n_function_tractorFrontloader")) {
+						base.extra += "front loader"
+					}
+					base
+
 				}
 			}
 		case Vector("vehicle", "motorized", "motorConfigurations", "motorConfiguration") => withErrorLog(p) {
 			(e: Elem) =>
+				if(!firstEngineOnly || (firstEngineOnly && base.engines.isEmpty)) {
 					base.engines += Engine(e \@ "name", e \ "@hp", base.price + e \ "@price")
-					null
+				}
+				null
 		}
 		case Vector("vehicle", "cylindered", "movingParts") => withErrorLog(p) {
 			(e: Elem) =>
